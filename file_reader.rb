a66e7a632dfd6f9739e44ac1010ac2e7d9808b94
Alexander Frankel <alexxander.frankel@gmail.com>
2014-05-03 16:01:14 -0400

    refactored file reader and mta status db

--- file_reader.rb
+++ file_reader.rb
@@ -5,41 +5,44 @@ require 'open-uri'
 class FileReader
 
 	MTA_FILE = 'http://web.mta.info/status/serviceStatus.txt'
-	@parsed_file = Nokogiri::XML::Document.new
 
-	def self.read_file
+	def self.parse
+		new.parse_file
+	end
+
+	def initialize
+	  @parsed_file = Nokogiri::XML::Document.new
+	end
+
+	def parse_file
+		read_file
+		Hash[update_line.zip update_status]
+	end
+
+	def read_file
 		update_parsed_file(Nokogiri::XML(open(MTA_FILE)))
 	end
 
-	def self.update_line
+	def update_line
 		view_parsed_file.xpath("//name").collect do |name|
 			name.children.text
 		end
 	end
 
-	def self.update_status
+	def update_status
 		view_parsed_file.xpath("//status").collect do |status|
 			status.children.text
 		end
 	end
 
-	# private
+	private
 
-	def self.update_parsed_file(new_file)
+	def update_parsed_file(new_file)
 		@parsed_file = new_file
 	end
 
-	def self.view_parsed_file
+	def view_parsed_file
 		@parsed_file
-	end
-
-end
-
-class StatusUpdate
-
-	def self.get_status_update
-		FileReader.read_file
-		Hash[FileReader.update_line.zip FileReader.update_status]
 	end
 
 end
--- mta_status_db.rb
+++ mta_status_db.rb
@@ -2,16 +2,32 @@ require_relative 'file_reader.rb'
 require 'pg'
 
 class UpdateDatabase
-	DATABASE_NAME = 'mta_status_updates'
 
-	def self.update_database
+	attr_reader :database_name, :db_connection
+
+	def self.update!
+		new.perform_update
+	end
+
+	def initialize(database_name = 'mta_status_updates')
+		@database_name = database_name
+		@db_connection = PG.connect(database_name)
+	end
+
+	def perform_update
+		database_utilities
+		create_table
+		insert_data
+		display_data
+	end
+
+	def database_utilities
 		ignore_errors = "/dev/null 2>&1"
-		`createdb #{DATABASE_NAME} #{ignore_errors}`
-
-		db_connection = PG.connect( dbname: DATABASE_NAME )
-
+		`createdb #{database_name} #{ignore_errors}`
 		db_connection.exec("drop table if exists trains;")
+	end
 
+	def create_table
 		db_connection.exec(<<-SQL
 		  create table trains
 		  (
@@ -21,18 +37,24 @@ class UpdateDatabase
 		  );
 		  SQL
 		)
+	end
 
-		StatusUpdate.get_status_update.each do |train, cur_status|
+	def insert_data
+		FileReader.parse.each do |train, cur_status|
 			db_connection.exec(<<-SQL
 					insert into trains (line, status)
 					values ('#{train}','#{cur_status}');
 				SQL
 				)
 		end
+	end
 
+	def display_data
 		results = db_connection.exec(<<-SQL
 			select * from trains;
 		SQL
 		)
 	end
 end
+
+p UpdateDatabase.update!
